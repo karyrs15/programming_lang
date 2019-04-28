5b741288d89e3992c4f01792d44866dc168305d7
Karina Reyes <a01700666@itesm.mx>
2019-04-27 23:52:18 -0500

    Add cumulative for interest

--- amortization.rkt
+++ amortization.rkt
@@ -36,11 +36,11 @@
 ; test
 ;(capital_to_be_paid 1000000 74210.87)
 
-(define (balance last amortization)
+(define (capital last amortization)
   (+ last amortization))
 
 ; test
-;(balance 0 74210.87)
+;(capital 0 74210.87)
 
 (define (total_payment loan months ir)
   (* (payment loan months ir) months))
@@ -52,18 +52,19 @@
 (define lst_cap_to_be_paid_y '()) 
 
 (define (amortization_table loan months ir)
-  (amortization_aux 12 0 (payment loan months ir) 0 0 loan 0 ir))
+  (amortization_aux 12 0 (payment loan months ir) 0 0 0 loan 0 ir))
 
-(define (amortization_aux months months_count pmt actual_interest amt cap_tb_paid cap_paid ir)
+(define (amortization_aux months months_count pmt actual_interest acum_interest amt cap_tb_paid cap_paid ir)
+  (writeln acum_interest)
   (set! lst_months (append lst_months (list months_count)))
   (set! lst_cap_to_be_paid_y (append lst_cap_to_be_paid_y (list cap_tb_paid)))
   (cond
     [(>= months_count months) #t]
     [else (amortization_aux months (+ months_count 1) pmt
-                 (interest cap_tb_paid ir)
+                 (interest cap_tb_paid ir) (+ (interest cap_tb_paid ir) acum_interest)
                  (amortization pmt (interest cap_tb_paid ir))
                  (capital_to_be_paid cap_tb_paid (amortization pmt (interest cap_tb_paid ir)))
-                 (balance cap_paid (amortization pmt (interest cap_tb_paid ir))) ir)]))
+                 (capital cap_paid (amortization pmt (interest cap_tb_paid ir))) ir)]))
 
 ; test
 ;(amortization_table 1000000 12 25)
@@ -92,17 +93,16 @@
   (amortization_table
    (string->number (send loan_field get-value))
    (string->number (send months_field get-value))
-   (string->number (send ir_field get-value))))
+   (string->number (send ir_field get-value)))
+  (new button% [parent frame]
+             [label "Graph"]
+             [callback main_graph]))
 
 (define (main_graph button event)
   (amortization_plot))
 
 (new button% [parent frame]
-             [label "Table"]
+             [label "Show results"]
              [callback main])
-
-(new button% [parent frame]
-             [label "Graph"]
-             [callback main_graph])
 
 (send frame show #t)
